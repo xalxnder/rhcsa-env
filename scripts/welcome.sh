39c0341726d88d26d95a5e56c27941737f889f1a
Xavier Alexander <xalexander51@gmail.com>
2023-09-21 05:27:05 -0400

    Add Scripts and Test Files

---
+++ scripts/welcome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "
+# Server Info
+Hostname - `hostname`
+User: `whoami`
+
+# How To Gain Root Access
+'sudo' or 'sudo su -'
+
+# Repo Location
+AppStream: http://repos.example.com
+BaseOS: http://repos.example.com
+"
---
+++ scripts/welcome_repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "
+You're probably here because you are unable to access the repos :) 
+
+# Server Info
+Hostname - `hostname`
+User: `whoami`
+
+# How To Gain Root Access
+'sudo' or 'sudo su -'
+
+"
